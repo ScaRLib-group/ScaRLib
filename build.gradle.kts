d66e4ed36c870fba7d683631d6fd1caf277afee9
dom <davide.domini2@unibo.it>
2023-01-28 14:58:56 +0100

    chore: init project

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+}
+
+group = "it.unibo.scarlib"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
