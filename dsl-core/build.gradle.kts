908bb28d93f63c569592df25a9916f1a409b3693
dom <davide.domini2@unibo.it>
2023-01-28 17:38:19 +0100

    build: add scalatest dependency

--- dsl-core/build.gradle.kts
+++ dsl-core/build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 
 dependencies {
     implementation("org.scala-lang:scala3-library_3:3.0.1")
-    testImplementation("g.scalatest:scalatest_3:3.2.9")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.scalatest:scalatest_3:3.2.15")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
