90fc6f7aad6f85bd7728fe77e83ae9387a8c00f9
dom <davide.domini2@unibo.it>
2023-01-29 17:15:47 +0100

    feat: add agent dsl support

--- dsl-core/src/main/scala/it/unibo/scarlib/dsl/Environment.scala
+++ dsl-core/src/main/scala/it/unibo/scarlib/dsl/Environment.scala
@@ -7,9 +7,11 @@ class Environment:
   private var rewardFunction: RewardFunction = _
   private var actionSpace: Set[Action] = Set.empty
   private var variables: Map[String, Double] = Map.empty
+  private var agent: Class[Agent] = _
 
   def setRewardFunction(rf: RewardFunction) = rewardFunction = rf
   def setActions(set: MSet[Action]):Unit = actionSpace = collection.immutable.Set[Action](set.toSeq:_*)
   def setVariables(map: MMap[String, Double]):Unit = variables = map.toMap
+  def setAgent(a: Class[Agent]) = agent = a
 
   override def toString: String = s"rf: ${rewardFunction}, as: ${actionSpace}, vars: ${variables}"
