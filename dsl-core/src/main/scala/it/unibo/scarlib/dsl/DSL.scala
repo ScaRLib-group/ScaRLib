727c723ef7a2c7ce2dbd4e26500d4c22bb68084e
dom <davide.domini2@unibo.it>
2023-01-29 16:29:33 +0100

    feat: add variables dsl support

--- dsl-core/src/main/scala/it/unibo/scarlib/dsl/DSL.scala
+++ dsl-core/src/main/scala/it/unibo/scarlib/dsl/DSL.scala
@@ -1,6 +1,8 @@
 package it.unibo.scarlib.dsl
 
 import scala.collection.mutable.{Set => MSet}
+import scala.collection.mutable.{Map => MMap}
+
 
 object DSL:
   def environment(init: Environment ?=> Unit) =
@@ -11,11 +13,21 @@ object DSL:
   def rewardFunction(init: Unit ?=> RewardFunction)(using env: Environment) =
     given unit: Unit = ()
     env.setRewardFunction(init)
-  
+
   def actions(init: MSet[Action] ?=> Unit)(using env: Environment) =
     given set: MSet[Action] = MSet.empty
     init
     env.setActions(set)
-  
+
   def action(a: Action)(using set: MSet[Action]) =
     set.add(a)
+
+  def variables(init: MMap[String, Double] ?=> Unit )(using env: Environment) =
+    given map: MMap[String, Double] = MMap.empty
+    init
+    env.setVariables(map)
+
+  def variable(variableName: String, variableValue: Double)(using map: MMap[String, Double]) =
+    map.addOne((variableName, variableValue))
+
+  def v(variableName: String, variableValue: Double)(using map: MMap[String, Double]) = variable(variableName, variableValue)
--- dsl-core/src/main/scala/it/unibo/scarlib/dsl/Environment.scala
+++ dsl-core/src/main/scala/it/unibo/scarlib/dsl/Environment.scala
@@ -1,28 +1,15 @@
 package it.unibo.scarlib.dsl
 
 import scala.collection.mutable.{Set => MSet}
+import scala.collection.mutable.{Map => MMap}
 
 class Environment:
   private var rewardFunction: RewardFunction = _
   private var actionSpace: Set[Action] = Set.empty
+  private var variables: Map[String, Double] = Map.empty
+
   def setRewardFunction(rf: RewardFunction) = rewardFunction = rf
   def setActions(set: MSet[Action]):Unit = actionSpace = collection.immutable.Set[Action](set.toSeq:_*)
+  def setVariables(map: MMap[String, Double]):Unit = variables = map.toMap
 
-  override def toString: String = s"rf: ${rewardFunction}, as: ${actionSpace}"
-/*
-def environment(init: Environment ?=> Unit) =
-    given env: Environment = Environment()
-    init
-    env
-
-def rewardFunction(init: Unit ?=> RewardFunction)(using env: Environment) =
-  given unit: Unit = ()
-  env.setRewardFunction(init)
-
-def actions(init: MSet[Action] ?=> Unit)(using env: Environment) =
-  given set: MSet[Action] = MSet.empty
-  init
-  env.setActions(set)
-
-def action(a: Action)(using set: MSet[Action]) =
-  set.add(a)*/
+  override def toString: String = s"rf: ${rewardFunction}, as: ${actionSpace}, vars: ${variables}"
