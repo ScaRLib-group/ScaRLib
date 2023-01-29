dad5827500d4c877f5f87d4ad9390f42302fe553
dom <davide.domini2@unibo.it>
2023-01-29 19:40:01 +0100

    chore: add git hook for conventional commits

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.1"
+}
+
 rootProject.name = "ScaRLib"
 include("dsl-core")
+
+gitHooks {
+    commitMsg {
+        conventionalCommits()
+    }
+    createHooks(true)
+}
