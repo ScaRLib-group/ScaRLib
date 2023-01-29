09c1d1d8d7b26a934e0586c5f9c29edd9bab8a6c
dom <davide.domini2@unibo.it>
2023-01-29 21:20:23 +0100

    test: add a simple test

---
+++ dsl-core/src/test/scala/it/unibo/scarlib/dsl/EnvironmentTest.scala
@@ -0,0 +1,23 @@
+package it.unibo.scarlib.dsl
+
+import org.junit.runner.RunWith
+import org.scalatestplus.junit.JUnitRunner
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+
+@RunWith(classOf[JUnitRunner])
+class EnvironmentTest extends AnyFlatSpec with Matchers{
+  "Simple test" should "success" in {
+      1 shouldBe 1
+  }
+}
+
+
+import org.scalatest.funsuite.AnyFunSuite
+@RunWith(classOf[JUnitRunner])
+class AppSuite extends AnyFunSuite {
+  test("App has a greeting") {
+    assert(1 == 1)
+  }
+}
