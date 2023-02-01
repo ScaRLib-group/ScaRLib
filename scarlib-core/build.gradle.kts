53b8227bf5443ae022b7c1c52aba19badb14f680
dom <davide.domini2@unibo.it>
2023-02-01 16:40:51 +0100

    build: add scala plugin

--- scarlib-core/build.gradle.kts
+++ scarlib-core/build.gradle.kts
@@ -1,17 +1,22 @@
 plugins {
     java
+    scala
 }
 
 group = "it.unibo.scarlib"
-version = "unspecified"
+
+scala {
+    zincVersion.set("1.6.1")
+}
+
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation("org.scala-lang:scala3-library_3:3.2.2")
+    testImplementation("junit:junit:4.13.2")
 }
 
 tasks.getByName<Test>("test") {
