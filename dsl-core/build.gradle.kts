1623a4d101bc0ca2a8dee9f7535e4834a08a4be0
dom <davide.domini2@unibo.it>
2023-02-02 10:07:34 +0100

    build: add scalactic dependency

--- dsl-core/build.gradle.kts
+++ dsl-core/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     scala
     application
+    //id("com.github.maiflai.scalatest") version "0.32"
 }
 
 group = "it.unibo.scarlib"
@@ -20,6 +21,8 @@ dependencies {
     testImplementation("org.scalatest:scalatest_2.13:3.3.0-SNAP3")
     testImplementation("org.scalatestplus:junit-4-13_3:3.2.15.0")
     testRuntimeOnly("org.scala-lang.modules:scala-xml_3:2.1.0")
+    testImplementation("org.scalactic:scalactic_2.13:3.3.0-SNAP3")
+    //testRuntimeOnly("com.vladsch.flexmark:flexmark-all:0.35.10")
 }
 
 /*tasks.getByName<Test>("test") {
