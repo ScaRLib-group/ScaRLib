ff3a7054a53b954077b96918719bc98b86fa3418
dom <davide.domini2@unibo.it>
2023-02-03 17:04:05 +0100

    fix: fix a typo in replaybuffer name

--- scarlib-core/src/main/scala/it/unibo/scarlib/core/model/ReplayBuffer.scala
+++ scarlib-core/src/main/scala/it/unibo/scarlib/core/model/ReplayBuffer.scala
@@ -4,16 +4,16 @@ import scala.util.Random
 
 case class Experience[State, Action](actualState: State, action: Action, reward: Double, nextState: State)
 
-trait ReplyBuffer[State, Action]:
+trait ReplayBuffer[State, Action]:
   def insert(actualState: State, action: Action, reward: Double, nextState: State): Unit
   def reset: Unit
   def subsample(batchSize: Int): Seq[Experience[State, Action]]
 
-object ReplyBuffer:
-  def apply[S, A](size: Int): ReplyBuffer[S, A] =
+object ReplayBuffer:
+  def apply[S, A](size: Int): ReplayBuffer[S, A] =
     new BoundedQueue[S, A](size)
 
-  private class BoundedQueue[State, Action](size: Int) extends ReplyBuffer[State, Action]:
+  private class BoundedQueue[State, Action](size: Int) extends ReplayBuffer[State, Action]:
 
     private var queue: Seq[Experience[State, Action]] = Seq.empty
 
@@ -24,14 +24,14 @@ object ReplyBuffer:
       Random(42).shuffle(queue).take(batchSize)
 
 object BufferTest extends App:
-  val buffer = ReplyBuffer[Double, Int](10)
+  val buffer = ReplayBuffer[Double, Int](10)
   buffer.insert(1.0, 1, 10, 1.3)
   buffer.insert(1.3, 2, -1, 1.3)
   buffer.insert(1.3, 4, 2, 1.5)
   print(buffer.subsample(3))
 
 object BufferTestMaxSize extends App:
-  val buffer = ReplyBuffer[Double, Int](10)
+  val buffer = ReplayBuffer[Double, Int](10)
   buffer.insert(1.0, 1, 10, 1.3)
   buffer.insert(1.1, 2, -1, 1.3)
   buffer.insert(1.2, 4, 2, 1.5)
