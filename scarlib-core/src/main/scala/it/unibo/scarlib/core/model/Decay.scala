bf409f577279d0db405c56312769bfc4fc33cbac
dom <davide.domini2@unibo.it>
2023-02-13 15:33:49 +0100

    feat: add bound to exp decay

--- scarlib-core/src/main/scala/it/unibo/scarlib/core/model/Decay.scala
+++ scarlib-core/src/main/scala/it/unibo/scarlib/core/model/Decay.scala
@@ -4,13 +4,14 @@ trait Decay[T]:
   def update: Unit
   def value: T
 
-class ExponentialDecay(initialValue: Double, rate: Double) extends Decay[Double]:
+class ExponentialDecay(initialValue: Double, rate: Double, bound: Double) extends Decay[Double]:
 
   private var elapsedTime: Int = 0
 
   override def update: Unit = elapsedTime = elapsedTime + 1
-  override def value: Double = initialValue * Math.pow(1-rate, elapsedTime)
-
+  override def value: Double =
+    val v = initialValue * Math.pow(1-rate, elapsedTime)
+    if v > bound then v else bound
 
 class ConstantDecay(initialValue: Double) extends Decay[Double]:
   override def update: Unit = ()
