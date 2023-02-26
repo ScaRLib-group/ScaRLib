7aa6954c504a41f613e95de10e11b64db1d34cd2
dom <davide.domini2@unibo.it>
2023-02-26 20:40:31 +0100

    fix: fix environment yaml specification

--- alchemist-scafi/src/main/scala/it/unibo/experiment/ScafiAgent.scala
+++ alchemist-scafi/src/main/scala/it/unibo/experiment/ScafiAgent.scala
@@ -1,11 +1,17 @@
 package it.unibo.experiment
 
+import it.unibo.alchemist.model.implementations.molecules.SimpleMolecule
+import it.unibo.alchemist.model.implementations.timedistributions.DiracComb
+import it.unibo.experiment.Actions.{East, North, South, West}
 import it.unibo.scafi.ScafiProgram
 import it.unibo.scafi.space.Point3D
-import it.unibo.scarlib.core.model.State
+import it.unibo.scarlib.core.model.{Action, NoAction, State}
+
+import _root_.scala.jdk.CollectionConverters._
 
 class ScafiAgent extends ScafiProgram {
     override protected def computeState(): State = {
+        makeActions()
         val distances = excludingSelf
           .reifyField(nbrVector())
           .toList
@@ -14,5 +20,21 @@ class ScafiAgent extends ScafiProgram {
           .map(point => (point.x, point.y))
           .take(3)
         MyState(distances, (currentPosition()._1, currentPosition()._2))
+    }
+
+    private def makeActions(): Unit = {
+      val dt = 1
+      alchemistEnvironment.getNodes.iterator().asScala.toList.foreach(
+        agent => {
+            val action = agent.getConcentration(new SimpleMolecule("action"))
+            action match {
+                case NoAction => // do nothing
+                case North => alchemistEnvironment.moveNodeToPosition(agent, alchemistEnvironment.getPosition(agent).plus(Array(0.0, dt)))
+                case South => alchemistEnvironment.moveNodeToPosition(agent, alchemistEnvironment.getPosition(agent).plus(Array(0.0, -dt)))
+                case West => alchemistEnvironment.moveNodeToPosition(agent, alchemistEnvironment.getPosition(agent).plus(Array(-dt, 0.0)))
+                case East => alchemistEnvironment.moveNodeToPosition(agent, alchemistEnvironment.getPosition(agent).plus(Array(dt, 0.0)))
+            }
+        }
+      )
     }
 }
