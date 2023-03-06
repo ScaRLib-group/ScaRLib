79dea6a6269895296c07ab6dab1e0cda1f053c04
dom <davide.domini2@unibo.it>
2023-03-06 19:19:20 +0100

    feat: implement new nn

---
+++ scarlib-core/src/main/scala/it/unibo/scarlib/core/neuralnetwork/SeqNN.scala
@@ -0,0 +1,19 @@
+package it.unibo.scarlib.core.neuralnetwork
+
+import it.unibo.scarlib.core.neuralnetwork.TorchSupport.{neuralNetworkModule => nn}
+import me.shadaj.scalapy.py
+
+object SeqNN extends DQN {
+  def apply(input: Int, hidden: Int, output: Int): py.Dynamic =
+    nn().Sequential(
+      nn().Linear(input, hidden).cuda(),
+      nn().ReLU().cuda(),
+      nn().Linear(hidden, hidden).cuda(),
+      nn().ReLU().cuda(),
+      nn().Linear(hidden, hidden).cuda(),
+      nn().ReLU().cuda(),
+      nn().Linear(hidden, hidden).cuda(),
+      nn().ReLU().cuda(),
+      nn().Linear(hidden, output).cuda()
+    ).cuda()
+}
