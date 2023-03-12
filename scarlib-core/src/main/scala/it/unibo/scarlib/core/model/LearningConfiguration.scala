f8974ed5796fa799041345bd8bdd9377c9cce643
dom <davide.domini2@unibo.it>
2023-03-12 15:10:46 +0100

    feat: add default values for learning configuration parameters

--- scarlib-core/src/main/scala/it/unibo/scarlib/core/model/LearningConfiguration.scala
+++ scarlib-core/src/main/scala/it/unibo/scarlib/core/model/LearningConfiguration.scala
@@ -1,9 +1,9 @@
 package it.unibo.scarlib.core.model
 
-case class LearningConfiguration[T](
-    epsilon: Decay[T],
-    gamma: Double,
-    learningRate: Double,
-    batchSize: Int,
-    updateEach: Int
+case class LearningConfiguration(
+    epsilon: Decay[Double] = new ExponentialDecay(0.9, 0.1, 0.01),
+    gamma: Double = 0.9,
+    learningRate: Double = 0.0005,
+    batchSize: Int = 32,
+    updateEach: Int = 100
     )
