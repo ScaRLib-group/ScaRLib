4171cabd0dad51aa845a8fec01ebc28994b14959
dom <davide.domini2@unibo.it>
2023-03-13 11:12:19 +0100

    refactor: rename dnq factory

--- scarlib-core/src/main/scala/it/unibo/scarlib/core/model/LearningConfiguration.scala
+++ scarlib-core/src/main/scala/it/unibo/scarlib/core/model/LearningConfiguration.scala
@@ -1,6 +1,6 @@
 package it.unibo.scarlib.core.model
 
-import it.unibo.scarlib.core.neuralnetwork.DQNFactory
+import it.unibo.scarlib.core.neuralnetwork.DQNAbstractFactory
 
 case class LearningConfiguration(
     epsilon: Decay[Double] = new ExponentialDecay(0.9, 0.1, 0.01),
@@ -8,5 +8,5 @@ case class LearningConfiguration(
     learningRate: Double = 0.0005,
     batchSize: Int = 32,
     updateEach: Int = 100,
-    dqnFactory: DQNFactory
+    dqnFactory: DQNAbstractFactory
     )
