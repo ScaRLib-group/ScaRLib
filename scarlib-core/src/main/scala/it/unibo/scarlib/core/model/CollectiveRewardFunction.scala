cd8065cc294a3b683ffac2fb7eef35744175a766
dom <davide.domini2@unibo.it>
2023-04-15 20:09:22 +0200

    chore: add licence note in all scala files

--- scarlib-core/src/main/scala/it/unibo/scarlib/core/model/CollectiveRewardFunction.scala
+++ scarlib-core/src/main/scala/it/unibo/scarlib/core/model/CollectiveRewardFunction.scala
@@ -1,3 +1,12 @@
+/*
+ * ScaRLib: A Framework for Cooperative Many Agent Deep Reinforcement learning in Scala
+ * Copyright (C) 2023, Davide Domini, Filippo Cavallari and contributors
+ * listed, for each module, in the respective subproject's build.gradle.kts file.
+ *
+ * This file is part of ScaRLib, and is distributed under the terms of the
+ * GNU General Public License as described in the file LICENSE in the ScaRLin distribution's top directory.
+ */
+
 package it.unibo.scarlib.core.model
 
 trait CollectiveRewardFunction extends RewardFunction
--- scarlib-core/src/main/scala/it/unibo/scarlib/core/model/SimpleRF.scala
+++
@@ -1,8 +0,0 @@
-
-package it.unibo.scarlib.core.model
-/*
->>>>>>> 08e81e3 (feat: add support for DTDE learning)
-class SimpleRF extends CollectiveRewardFunction:
-
-    override def compute(states: Seq[State], updates: Int): Double = ???
-*/
