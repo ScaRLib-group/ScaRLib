110127cd416d748821460eaf66ee9c1aa36989cc
dom <davide.domini2@unibo.it>
2023-11-08 17:38:22 +0100

    fix(ReplayBuffer): fix generics

--- scarlib-core/src/main/scala/it/unibo/scarlib/core/model/ReplayBuffer.scala
+++ scarlib-core/src/main/scala/it/unibo/scarlib/core/model/ReplayBuffer.scala
@@ -19,22 +19,22 @@ import collection.mutable.ArrayDeque
  * @param reward the reward earned by the agent after taking the action from the actual state
  * @param nextState the state in which the environment goes into after the action taken by the agent
  */
-case class Experience[State, Action](actualState: State, action: Action, reward: Double, nextState: State)
+case class Experience[S <: State, A <: Action](actualState: S, action: A, reward: Double, nextState: S)
 
 /** The container of agents experience */
-trait ReplayBuffer[State, Action]{
+trait ReplayBuffer[S <: State, A <: Action]{
 
   /** Inserts new experience */
-  def insert(actualState: State, action: Action, reward: Double, nextState: State): Unit
+  def insert(actualState: S, action: A, reward: Double, nextState: S): Unit
 
   /** Empty the buffer */
   def reset(): Unit
 
   /** Gets a sub-sample of the experience stored by the agents */
-  def subsample(batchSize: Int): Seq[Experience[State, Action]]
+  def subsample(batchSize: Int): Seq[Experience[S, A]]
 
   /** Gets all the experience stored by the agents */
-  def getAll(): Seq[Experience[State, Action]]
+  def getAll(): Seq[Experience[S, A]]
 
   /** Gets the buffer size */
   def size(): Int
@@ -42,23 +42,23 @@ trait ReplayBuffer[State, Action]{
 }
 
 object ReplayBuffer{
-  def apply[S, A](size: Int): ReplayBuffer[S, A] = {
+  def apply[S <: State, A <: Action](size: Int): ReplayBuffer[S, A] = {
     new BoundedQueue[S, A](size, 42)
   }
 
-  private class BoundedQueue[State, Action](bufferSize: Int, seed: Int) extends ReplayBuffer[State, Action]{
+  private class BoundedQueue[S <: State, A <: Action](bufferSize: Int, seed: Int) extends ReplayBuffer[S, A]{
 
-    private var queue: ArrayDeque[Experience[State, Action]] = ArrayDeque.empty
+    private var queue: ArrayDeque[Experience[S, A]] = ArrayDeque.empty
 
-    override def reset(): Unit = queue = ArrayDeque.empty[Experience[State, Action]]
+    override def reset(): Unit = queue = ArrayDeque.empty[Experience[S, A]]
 
-    override def insert(actualState: State, action: Action, reward: Double, nextState: State): Unit =
+    override def insert(actualState: S, action: A, reward: Double, nextState: S): Unit =
       queue = (queue :+ Experience(actualState, action, reward, nextState)).takeRight(bufferSize)
 
-    override def subsample(batchSize: Int): Seq[Experience[State, Action]] =
+    override def subsample(batchSize: Int): Seq[Experience[S, A]] =
       new Random(seed).shuffle(queue).take(batchSize).toSeq
 
-    override def getAll(): Seq[Experience[State, Action]] = queue.toSeq
+    override def getAll(): Seq[Experience[S, A]] = queue.toSeq
 
     override def size(): Int = queue.size
   }
--- scarlib-core/src/test/scala/it/unibo/scarlib/core/model/BufferTest.scala
+++ scarlib-core/src/test/scala/it/unibo/scarlib/core/model/BufferTest.scala
@@ -14,22 +14,28 @@ import org.scalatestplus.junit.JUnitRunner
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+class MyState(id: Int) extends State {
+  override def isEmpty(): Boolean = false
+}
+
+class MyAction(id: Int) extends Action
+
+
 @RunWith(classOf[JUnitRunner])
 class BufferTest extends AnyFlatSpec with Matchers:
   "Insert method" should "increase buffer size when it is not full" in {
-    val buffer = ReplayBuffer[Double, Int](10)
-    buffer.insert(1.0, 1, 10, 1.3)
+    val buffer = ReplayBuffer[MyState, MyAction](10)
+    buffer.insert(new MyState(1), new MyAction(1), 10, new MyState(1))
     buffer.getAll.size shouldBe 1
   }
 
   "Insert method" should "not overflow buffer size" in {
-    val buffer = ReplayBuffer[Double, Int](5)
-    buffer.insert(1.0, 1, 10, 1.3)
-    buffer.insert(1.1, 1, 10, 1.3)
-    buffer.insert(1.2, 1, 10, 1.3)
-    buffer.insert(1.3, 1, 10, 1.3)
-    buffer.insert(1.4, 1, 10, 1.3)
-    buffer.insert(1.5, 1, 10, 1.3)
+    val buffer = ReplayBuffer[MyState, MyAction](5)
+    buffer.insert(new MyState(1), new MyAction(1), 10, new MyState(1))
+    buffer.insert(new MyState(2), new MyAction(2), 10, new MyState(2))
+    buffer.insert(new MyState(3), new MyAction(3), 10, new MyState(3))
+    buffer.insert(new MyState(4), new MyAction(4), 10, new MyState(4))
+    buffer.insert(new MyState(5), new MyAction(5), 10, new MyState(5))
     buffer.getAll.size shouldBe 5
   }
 
