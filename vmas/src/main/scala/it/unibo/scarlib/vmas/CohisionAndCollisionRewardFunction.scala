3b678d1d21eb60ad3576dc95d1119257791917aa
Filippo <filippo.cavallari99@gmail.com>
2024-01-02 11:54:10 +0100

    chore: delete unnecessary class

--- vmas/src/main/scala/it/unibo/scarlib/vmas/CohisionAndCollisionRewardFunction.scala
+++
@@ -1,7 +0,0 @@
-package it.unibo.scarlib.vmas
-
-import it.unibo.scarlib.core.model.{Action, RewardFunction, State}
-
-class CohisionAndCollisionRewardFunction extends RewardFunction{
-    override def compute(currentState: State, action: Action, newState: State): Double = ???
-}
